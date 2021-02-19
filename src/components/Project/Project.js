96c70eed98c16964cd9ab1dfc00f11bab78c44f0
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-19 22:24:46 +0530

    screen size ui fixes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,18 +4,23 @@ import './Project.scss'
 const Project = ({ project }) => {
     return (
         <div className="project">
-            <div className="project-top">
-                <h1 className="project-title" >{project.name}</h1>
-                <a target='_blank' href={project.repo} className="project-repo">
-                    <img src="./assets/github.svg" alt={project.name}/>
-                </a>
-            </div>
+            <h1 className="project-title" >{project.name}</h1>
+
             <div className="project-image">
                 <img src={`./image/${project.image}.png`} alt={project.name} />
             </div>
+
+            <div className="project-repo">
+                <a target='_blank' href={project.repo} className="repo">
+                    <img src="./assets/github.svg" alt={project.name}/>
+                    <p>Github</p>
+                </a>
+            </div>
+
+
             <div className="deploy">
                 <a target='_blank' href={project.deploy.netlify} >Netlify</a>
-                <a target='_blank'  href={project.deploy.vercel} >Vercel</a>
+                <a target='_blank' href={project.deploy.vercel} >Vercel</a>
             </div>
         </div>
     )
